1643aade16efcef1ae4d732431afc92d4042442d
komarov <wikkomarik@gmail.com>
2022-06-02 09:47:20 +0100

    sprawdzenie liczby

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let n = 100;
+function check(n) {
+    if (n > 200) {
+        console.log("Liczba nie znajduje się w przedziale")
+    } if (n < 100) {
+        console.log("Liczba nie znajduje się w przedziale")
+    } else {
+        console.log("Liczba znajduje się w przedziale")
+    }
+}
+console.log(check(100))
